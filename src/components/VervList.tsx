57233d7d1e398adb8bdac6f421bc1ee91ce47519
Casper Faarlund <caspef@ntnu.no>
2024-04-27 19:24:18 +0200

    fix: design

--- src/components/VervList.tsx
+++ src/components/VervList.tsx
@@ -15,7 +15,7 @@ type VervListProps = {
 
 export default function VervList({ vervList }: VervListProps) {
   return (
-    <div className="container mx-auto p-4">
+    <div className="container mx-auto pb-4">
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
             {vervList.map((verv: Verv, index: number) => (
                 <VervCard
