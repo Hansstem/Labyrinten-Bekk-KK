2cd27a1b45856ac2417c036fec2a5958cca914ad
ROversand <96338299+ROversand@users.noreply.github.com>
2024-04-27 15:55:39 +0200

    feat: add baseline verv-list component

---
+++ src/components/VervList.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Button } from "@/components/ui/button"
+
+export default function VervList() {
+  return (
+    <div>
+      <h1>VervList</h1>
+      <Button>Click me</Button>
+    </div>
+  );
+}
