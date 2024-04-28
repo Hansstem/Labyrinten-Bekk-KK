9b2e192d06ec26f2887192e7b1224e724bf8d341
Casper Faarlund <caspef@ntnu.no>
2024-04-28 09:24:28 +0200

    feat. sleepy boys

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -10,7 +10,7 @@ import {
 } from "../components/ui/card"
 import {Button} from "../components/ui/button"
 import frontIllustration from "../assets/frontIllustration.svg"
-import pils from "../assets/pils.jpg"
+import pils from "../assets/trott.png"
 
 export default function LandingPage() {
   const navigate = useNavigate();
@@ -20,17 +20,9 @@ export default function LandingPage() {
       <div className="flex flex-col md:flex-row justify-between items-center gap-8 mb-6">
         
         <div className="flex-1">
-          <Card className="flex flex-row p-8 shadow-lg rounded-2xl">
-            <img className="w-full md:w-1/2 lg:w-1/3" src={pils} alt="Front Illustration" />
-            <div className="flex flex-col">
-              <CardHeader>
-                <CardTitle className="text-left text-8xl">Pils enjoyed: <b>8</b></CardTitle>
-              </CardHeader>
-              <CardContent className="text-left text-4xl">
-                <p>Nam, det var digg as</p>
-              </CardContent>
+          <Card className="flex p-8 shadow-lg rounded-2xl">
+            <img className="w-fulllg:w-1/3" src={pils} alt="Front Illustration" />
             
-            </div>
           </Card>
         </div>
       </div>
